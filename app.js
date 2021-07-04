d05cb3392bbfa7bc2133e01d9a55021d4b1e1af7
Rahul <pvrahul.271199@gmail.com>
2021-07-04 23:31:39 +0530

    adding files

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ const Key = require('./models/key')
 const method = require('method-override');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
+const flash = require('connect-flash');
 const key = require('./models/key');
 const app = express();
 require('./auth');
+
 const port = 80;
 
 mongoose.connect("mongodb://localhost:27017/options", {
@@ -21,10 +23,10 @@ mongoose.connect("mongodb://localhost:27017/options", {
         useFindAndModify: false
     })
     .then((res) => {
-        // // console.log("Database Connection Established")
+         console.log("Database Connection Established")
     })
     .catch((e) => {
-        // // console.log("Error Obtained")
+         console.log("Error Obtained")
     })
 
 
@@ -67,46 +69,37 @@ async function keyAvailable(req,res,next){
         next();
     }
     
-
-}
-function isFileavailable(req, res, next) {
-    const file1 = `${id}_key.txt`;
-    const file2 = `${id}_secret.txt`;
-    const boolean1 = fs.existsSync(file1);
-    const boolean2 = fs.existsSync(file2);
-    !boolean1 && !boolean2 ? next() : res.redirect('/analyze');
-    // // console.log(req.user)
 }
 
-function deleteFile(req, res, next) {
-    const id = req.user.id;
-    const file1 = `${id}_key.txt`;
-    const file2 = `${id}_secret.txt`;
-    fs.unlinkSync(file1);
-    fs.unlinkSync(file2);
-    next();
-}
 
-app.get('/request', isLogged, (req,res) => {
-    const id = req.user.id;
-    console.log(req.query);
-    const token = req.query.request_token;
-    const file = `${id}_token.txt`;
-    fs.writeFileSync(file, token)
-    // console.log(req.query.request_token);
-    res.render('order')
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope: ['email', 'profile']
+    })
+)
+
+app.get('/google/callback', //protected
+
+    passport.authenticate('google', {
+        successRedirect: '/analyze',
+        failureRedirect: '/auth/failed'
+    })
+)
+
+app.get('/auth/failed', (req, res) => { //protected
+    res.send('Auth Failed')
 })
 
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect("/")
+})
 
 app.get('/', (req, res) => {
-// console.log(req.user)
-// console.log(res.locals.user)
     if(req.user){
         const data = req.user;
         res.render('home',{data})
-
     } else {
-
         const data = {};
         res.render('home', {data})
     }
@@ -126,17 +119,8 @@ app.get('/details', isLogged, async (req, res) => {
     } else{
   console.log("Inside credentials")
     res.render('credentialsnew')
-}
-// // console.log(req.user)
-// // console.log(id)
-    // // console.log(`Hi ${data}`);
-    
+}    
 })
-
-app.get('/reset', deleteFile, (req, res) => {
-    res.redirect('/')
-})
-
 
 app.post('/details', isLogged,  async (req, res) => {
      const {
@@ -145,8 +129,6 @@ app.post('/details', isLogged,  async (req, res) => {
          secretkey
      } = req.body;
     const id = req.user.id;
-    console.log(req.body)
-// // console.log(id);
     const data = new Key({
         id,
         broker,
@@ -155,53 +137,28 @@ app.post('/details', isLogged,  async (req, res) => {
     })
     console.log(data)
     await data.save();
-    res.redirect('/analyze')
-    // res.render('credentials', {data})
+    
+    res.redirect('/analyze');
 });
     
-    // const url = `https://kite.trade/connect/login?api_key=${apikey}&v=3`
-    // fs.writeFileSync(usr_id + "_key.txt", apikey);
-    // fs.writeFileSync(usr_id + "_secret.txt", apisecret);
-    // res.redirect(url)
 app.delete('/key/delete/:id', async (req, res) => {
     const {
         id
     } = req.params;
-    // // console.log(id);
      const data = await Key.findByIdAndDelete(id);
-    // // // console.log(person)
-    // res.redirect('/basket')
-res.redirect("/details")
-
+    res.redirect("/details")
 })
-
-app.get('/auth/google',
-    passport.authenticate('google', {
-        scope: ['email', 'profile']
-    })
-)
-
-app.get('/google/callback', //protected
-
-    passport.authenticate('google', {
-        successRedirect: '/analyze',
-        failureRedirect: '/auth/failed'
-    })
-)
-
-
 
 app.get('/analyze', isLogged, keyAvailable,(req, res) => { //protected
     const user = req.user;
     const id = user.id;
     const request_token = req.query;
-
     res.render('analyze', {
         user
     });
 })
 
-app.post('/analyze', isLogged, async (req, res) => {
+app.post('/analyze', isLogged, keyAvailable,async (req, res) => {
     const {
         script,
         strikeprice,
@@ -216,10 +173,7 @@ app.post('/analyze', isLogged, async (req, res) => {
         exittime,
         sltocost
     } = req.body;
-    console.log(req.body)
-    // // console.log(option);
 
-    // // console.log(req.body)
     const user = req.user;
     const id = user.id;
     const person = new User({
@@ -238,11 +192,8 @@ app.post('/analyze', isLogged, async (req, res) => {
         sltocost
     })
     await person.save();
-    // // console.log(req.body)
-    res.redirect('/analyze')
+    res.redirect('/basket')
 })
-
-
 
 app.get('/basket', isLogged, keyAvailable, async (req, res) => {
     const id = req.user.id;
@@ -252,68 +203,55 @@ app.get('/basket', isLogged, keyAvailable, async (req, res) => {
     res.render('basket', {
         person
     })
-
 })
 
 app.delete('/delete/:id', async (req, res) => {
     const {
         id
     } = req.params;
-    // // console.log(id);
     const person = await User.findByIdAndDelete(id);
-    // // console.log(person)
     res.redirect('/basket')
-
 })
-app.get('/hi' , (req,res) => {
 
-    res.render('order')
-    })
 app.get('/edit/:id', async (req, res) => {
     const {
         id
     } = req.params;
     const person = await User.findById(id);
-    // // console.log(person)
     res.render('edit', {
         person
     })
 })
 
-
 app.put('/edit/:id', async (req, res) => {
     const {
         id
     } = req.params;
-    // // // // console.log(req.body);
-
+    
     const person = await User.findByIdAndUpdate(id, {
         ...req.body
     })
-    // // console.log(person)
     res.redirect('/basket')
-
 })
 
 app.get('/schedule',isLogged, async(req,res) => {
     const id = req.user.id;
-    // console.log(id);
     const user = await Key.find({id})
     const {broker, apikey, secretkey} = user[0];
     const url = `https://kite.trade/connect/login?api_key=${apikey}&v=3`
     res.redirect(url);
 })
 
-
-
-app.get('/auth/failed', (req, res) => { //protected
-    res.send('Auth Failed')
+app.get('/request', isLogged, (req,res) => {
+    const id = req.user.id;
+    console.log(req.query);
+    const token = req.query.request_token;
+    const file = `${id}_token.txt`;
+    fs.writeFileSync(file, token)
+    // console.log(req.query.request_token);
+    res.render('order')
 })
 
-app.get('/logout', (req, res) => {
-    req.logout();
-    res.redirect("/")
-})
 
 app.listen(port, () => {
  console.log(` Server listening at ${port}`)
